596f461e73ad26d05681f46a257c50bc290a9fae
Baris Gungor <gungorbaris41@gmail.com>
2023-10-03 13:45:23 +0300

    add searchView

--- app/src/main/java/com/barisgungorr/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/barisgungorr/ui/fragment/MainFragment.kt
@@ -1,10 +1,13 @@
 package com.barisgungorr.ui.fragment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView.OnQueryTextListener
 import androidx.navigation.Navigation
 import com.barisgungorr.data.entity.Notes
 import com.barisgungorr.todoapplication.R
@@ -36,7 +39,33 @@ class MainFragment : Fragment() {
             Navigation.findNavController(it).navigate(go)
         }
 
+        binding.searchView.setOnQueryTextListener(object :OnQueryTextListener{
+
+            override fun onQueryTextSubmit(query: String): Boolean {
+               search(query)
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                search(newText)
+                return true
+            }
+        })
+
+        binding.searchView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                // SearchView'e dokunulduğunda, metin düzenlemesi başlatılır
+                binding.searchView.isIconified = false
+            }
+            false
+        }
+
+
+
+
         return view
     }
 
+    fun search(searchWord: String) {
+        Log.e("Kişi Ara",searchWord)
+    }
 }
